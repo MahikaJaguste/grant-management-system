f9f67340a12e827c8f0c8ee2591efca6f09d3cfa
Mahika Jaguste <mahika.for.me@gmail.com>
2025-03-07 20:50:24 +0000

    basename in router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router basename="/grant-management-system">
+      <Router basename="https://mahikajaguste.github.io/grant-management-system">
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/professor" element={<ProfessorDashboard />} />
