fdc3a50ea1302c080a28310127b1d0ec27161ea7
Mahika Jaguste <mahika.for.me@gmail.com>
2025-03-07 14:13:00 +0000

    static flag for hosting on github pages

--- frontend/src/api/reviewService.js
+++ frontend/src/api/reviewService.js
@@ -1,11 +1,21 @@
-import axiosInstance from './axiosConfig';
+import axiosInstance, {STATIC_FLAG} from './axiosConfig';
+
+const staticReviewerNotifications = [
+  { id: 1,  title: "New Grant Application!", description: "New grant application with ID 3 is pending review." },
+];
 
 export const reviewApplication = async ({ id, status }) => {
+  if (STATIC_FLAG) {
+    return { status: "SUCCESS", grant: { id, status } };
+  }
   const response = await axiosInstance.post(`/grants/review/${id}`, { status });
   return response?.data;
 };
 
 export const fetchCommitteeNotifications = async () => {
+  if(STATIC_FLAG) {
+    return staticReviewerNotifications.reverse();
+  }
   const response = await axiosInstance.get("/notifications/reviewer");
   return response?.data?.notifications;
 }
